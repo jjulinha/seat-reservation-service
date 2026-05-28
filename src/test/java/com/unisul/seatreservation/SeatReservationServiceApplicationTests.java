8bb94cd6c5b1e4e0d6e927a71f84a466a6365b75
jjulinha <jodjulia03@gmail.com>
2026-05-28 14:41:49 -0300

    chore: setup inicial do projeto via Spring Initializr

---
+++ src/test/java/com/unisul/seatreservation/SeatReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisul.seatreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
